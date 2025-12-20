47b8db14cec10a4e00b83d24a82f500b0136e716
huynhvietchung <huynhvietchung2006@gmail.com>
2025-12-20 15:35:09 +0700

    stack+queue

--- MinRide-Backend/src/data_structures/StackNQueue/Stack.java
+++ MinRide-Backend/src/data_structures/StackNQueue/Stack.java
@@ -1,4 +1,48 @@
 package data_structures.StackNQueue;
 
 public class Stack {
+    private int[] stack;
+    private int top;
+
+    public Stack(int capacity) {
+        stack = new int[capacity];
+        top = -1;
+    }
+
+    // Thêm phần tử (push)
+    public void push(int x) {
+        if (top == stack.length - 1) {
+            System.out.println("Stack overflow");
+            return;
+        }
+        stack[++top] = x;
+    }
+
+    // Xóa phần tử (pop)
+    public int pop() {
+        if (top == -1) {
+            System.out.println("Stack underflow");
+            return -1;
+        }
+        return stack[top--];
+    }
+
+    // Xem phần tử trên cùng
+    public int peek() {
+        if (top == -1) return -1;
+        return stack[top];
+    }
+
+    // Kiểm tra rỗng
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    // Hiển thị
+    public void display() {
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+    }
 }
