854ab1c5f839d2c1e3282d903011d5c6462949cb
Nguyễn Hữu Huynh <sieudoge0@gmail.com>
2025-12-21 14:16:00 +0700

    Fix Customers

--- core/src/models/Customers.java
+++ core/src/models/Customers.java
@@ -1,11 +1,9 @@
 package models;
 
 public class Customers extends Person {
-    private String district;
 
-    public Customers(String id, String name, int x, int y, String district) {
+    public Customers(String id, String name, int x, int y) {
         super(id, name, x, y);
-        this.district = district;
     }
 
     @Override
